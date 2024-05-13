0a6c8bfcf4d73d33ee5efd12c8e6d43a82ede3d4
cl3arview-otg <nabil_romi@hotmail.com>
2024-05-13 15:09:39 +0100

    Added Database Initialization for User role.

--- src/main/java/org/advancedsr/Main.java
+++ src/main/java/org/advancedsr/Main.java
@@ -1,12 +1,27 @@
 package org.advancedsr;
 
+import org.advancedsr.entities.Role;
+import org.advancedsr.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(RoleRepository roleRepository) {
+        return args -> {
+            Role adminRole = Role.builder()
+                    .roleName("User")
+                    .permissions("Default Permissions.")
+                    .build();
+            roleRepository.save(adminRole);
+        };
     }
 }
